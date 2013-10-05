e420f1da51224d0e87dd05af117d078033187f25
Pat Cavit <pcavit@gmail.com>
2013-10-04 22:59:27 -0700

    Fixed + updated tests
    Had to open write stream before reading attribute, weirdly.

--- test/inliner.js
+++ test/inliner.js
@@ -10,7 +10,30 @@ var fs      = require("fs"),
 
 describe("JS Inliner", function() {
     describe("Main Module", function() {
-        it("should something something something", function(done) {
+        it("should inline files in html", function(done) {
+            var file   = "./test/specimens/test.html",
+                stream = fs.createReadStream(file, { encoding : "utf8" }),
+                root   = path.dirname(file);
+            
+            inliner(stream, { root : root }, function(err, results) {
+                var js;
+                
+                assert.ifError(err);
+                
+                js = results.toString("utf8");
+                
+                assert(js.indexOf("src=\"wooga.js\"") === -1);
+                assert(js.indexOf("src=\"/booga/fooga.js\"") === -1);
+                
+                assert(js.indexOf("<script >var wooga = \"I am wooga.js\";</script>") > -1);
+                assert(js.indexOf("<script >var fooga = \"I am /booga/fooga.js\";</script>") > -1);
+                assert(js.indexOf("<script src=\"biggun.js\"></script>") > -1);
+                
+                done();
+            });
+        });
+        
+        it("should inline files in ejs", function(done) {
             var file   = "./test/specimens/test.ejs",
                 stream = fs.createReadStream(file, { encoding : "utf8" }),
                 root   = path.dirname(file);
@@ -27,6 +50,7 @@ describe("JS Inliner", function() {
                 
                 assert(js.indexOf("<script >var wooga = \"I am wooga.js\";</script>") > -1);
                 assert(js.indexOf("<script >var fooga = \"I am /booga/fooga.js\";</script>") > -1);
+                assert(js.indexOf("<script src=\"biggun.js\"></script>") > -1);
                 
                 done();
             });
