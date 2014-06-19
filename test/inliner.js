b21a292a0ddc1542a625e7bc82e82055d0846c52
Pat Cavit <pcavit@gmail.com>
2014-06-19 11:53:44 -0700

    Tests are all busted

--- test/inliner.js
+++ test/inliner.js
@@ -10,7 +10,7 @@ var fs      = require("fs"),
 
 describe("JS Inliner", function() {
     describe("Main Module", function() {
-        it("should inline files in html", function(done) {
+        it.only("should inline files in html", function(done) {
             var file   = "./test/specimens/test.html",
                 stream = fs.createReadStream(file, { encoding : "utf8" }),
                 root   = path.dirname(file);
@@ -25,8 +25,8 @@ describe("JS Inliner", function() {
                 assert(js.indexOf("src=\"wooga.js\"") === -1);
                 assert(js.indexOf("src=\"/booga/fooga.js\"") === -1);
                 
-                assert(js.indexOf("<script >var wooga = \"I am wooga.js\";</script>") > -1);
-                assert(js.indexOf("<script >var fooga = \"I am /booga/fooga.js\";</script>") > -1);
+                assert(js.indexOf("<script>var wooga = \"I am wooga.js\";</script>") > -1);
+                assert(js.indexOf("<script>var fooga = \"I am /booga/fooga.js\";</script>") > -1);
                 assert(js.indexOf("<script src=\"biggun.js\"></script>") > -1);
                 
                 done();
@@ -48,9 +48,31 @@ describe("JS Inliner", function() {
                 assert(js.indexOf("src=\"wooga.js\"") === -1);
                 assert(js.indexOf("src=\"/booga/fooga.js\"") === -1);
                 
-                assert(js.indexOf("<script >var wooga = \"I am wooga.js\";</script>") > -1);
-                assert(js.indexOf("<script >var fooga = \"I am /booga/fooga.js\";</script>") > -1);
+                assert(js.indexOf("<script>var wooga = \"I am wooga.js\";</script>") > -1);
+                assert(js.indexOf("<script>var fooga = \"I am /booga/fooga.js\";</script>") > -1);
                 assert(js.indexOf("<script src=\"biggun.js\"></script>") > -1);
+                
+                done();
+            });
+        });
+        
+        it("should inline files in partial ejs", function(done) {
+            var file   = "./test/specimens/partial.ejs",
+                stream = fs.createReadStream(file, { encoding : "utf8" }),
+                root   = path.dirname(file);
+            
+            inliner(stream, { root : root }, function(err, results) {
+                var js;
+                
+                assert.ifError(err);
+                
+                js = results.toString("utf8");
+                
+                assert(js.indexOf("src=\"wooga.js\"") === -1);
+                assert(js.indexOf("src=\"/booga/fooga.js\"") === -1);
+                
+                assert(js.indexOf("<script>var wooga = \"I am wooga.js\";</script>") > -1);
+                assert(js.indexOf("<script>var fooga = \"I am /booga/fooga.js\";</script>") > -1);
                 
                 done();
             });
